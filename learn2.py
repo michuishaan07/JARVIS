611050231c35a19e6ca4c7bccec81ac88bea17e4
michuishaan07 <ishaanuiet@gmail.com>
2025-05-30 17:06:13 +0530

    Removed learn2.py

--- learn2.py
+++
@@ -1,8 +0,0 @@
-from googlesearch import search
-
-import webbrowser
-def google_search(query):
-    for url in search(query, num_results=1):
-        webbrowser.open(url)
-
-google_search("Python programming")
